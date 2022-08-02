3c935eb5203af75a13975fbc4dc705ac286c1361
Prajwal Koirala <koiralaprajwal07@gmail.com>
2022-08-02 13:44:24 +0545

    updated

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from tensorflow import keras
 from keras.preprocessing import image
 from flask import Flask, request, jsonify, render_template
 from detector import Comparer
+
 comparer = Comparer()
 
 app = Flask(__name__)
@@ -18,16 +19,10 @@ def home():
     return render_template('index.html')
 
 
-def dogorcat(img):
-
-    return str(my_response)
-
-
-
 @app.route('/ajax', methods=['POST'])
 def ajax():
     a = request.form['img']
-    r =  base64.b64decode(a.split(",")[1])
+    r = base64.b64decode(a.split(",")[1])
     image = Image.open(io.BytesIO(r))
     image = np.array(image)
     classes = comparer.find_match(image)
@@ -38,8 +33,6 @@ def ajax():
     }
     print(classes)
     return jsonify(my_response)
-
-
 
 
 app.run()
